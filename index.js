5b19e98606f7ec054f4678e8388eb94fd38e85f3
mokshali <mokshalibawiskar16@gmail.com>
2024-12-24 09:24:41 +0530

    add error handler err

--- index.js
+++ index.js
@@ -12,12 +12,19 @@ app.use(cors({
     origin:"http://localhost:5173",
     credentials:true
 }))
-
 app.use("/api/auth",require("./routes/auth.route"))
 app.use("/api/admin",adminProtected,require("./routes/admin.route"))
 app.use("/api/employee",require("./routes/employee.route"))
 app.use("*",(req,res)=>{
     res.status(404).json({message:"resourse not found"})
+})
+// express error handler👇
+app.use((err,req,res,next)=>{
+    if (err) {
+        console.log(err);
+        return res.status(500).json({message:"something went wrong"})
+        
+    }
 })
 
 mongoose.connect(process.env.MONGO_URL)
